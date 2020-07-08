e1d10b30c01d76900af6e2c09cab64f851e6cc2d
Azamat229 <azamatmambetaliev229@gmail.com>
2020-07-08 22:25:51 +0600

    testing8

--- my/my/settings.py
+++ my/my/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/Azamat228/azamat228.pythonanywhere.com/my/static'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = '/home/Azamat228/azamat228.pythonanywhere.com/my/static'
+STATIC_ROOT = os.path.join(BASE_DIR, '/home/Azamat228/azamat228.pythonanywhere.com/my/static')
